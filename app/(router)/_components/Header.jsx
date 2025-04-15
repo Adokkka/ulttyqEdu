34624ecc6200c9a785f3658e01d4a84b46e4d5c7
Adilett Kabdollauly <web.developer@isker.kz>
2025-04-15 11:03:24 +0500

    mob dev2

--- app/(router)/_components/Header.jsx
+++ app/(router)/_components/Header.jsx
@@ -8,24 +8,28 @@ import React from "react";
 function Header() {
   const { user, isLoaded } = useUser();
   return (
-    <div className="p-4 bg-white flex justify-between">
+    <div className="p-4 bg-white flex flex-col sm:flex-row gap-4 sm:justify-between sm:items-center shadow-sm">
       {/* Search bar  */}
       <div
         className="flex gap-2 border 
-        p-2
-        rounded-md"
+        p-2 rounded-md items-center w-full sm:w-auto"
       >
-        <Search className="h-5 w-5" />
-        <input type="text" placeholder="Іздеу..." className="outline-none" />
+        <Search className="h-5 w-5 text-gray-500" />
+        <input
+          type="text"
+          placeholder="Іздеу..."
+          className="outline-none w-full text-sm"
+        />
       </div>
+
       {/* Get Started Button & bell Icon */}
-      <div className="flex items-center gap-4">
+      <div className="flex items-center justify-between sm:justify-end gap-4 w-full sm:w-auto">
         <BellDot className="text-gray-500" />
         {isLoaded && user ? (
           <UserButton afterSignOutUrl="/courses" />
         ) : (
           <Link href={"/sign-in"}>
-            <Button>Тіркелу</Button>
+            <Button size="sm">Тіркелу</Button>
           </Link>
         )}
       </div>
