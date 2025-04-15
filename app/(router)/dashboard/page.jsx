e21d2c5388da209783c2dd309157ad58c56274ae
Adilett Kabdollauly <web.developer@isker.kz>
2025-04-15 11:05:34 +0500

    mob dev3

--- app/(router)/dashboard/page.jsx
+++ app/(router)/dashboard/page.jsx
@@ -1,44 +1,52 @@
-"use client"
-import { useUser } from '@clerk/nextjs'
-import React, { useEffect, useState } from 'react'
-import SideBanners from '../courses/_components/SideBanners';
-import WelcomeBannerDashboard from './_components/WelcomeBannerDashboard';
-import InProgressCourseList from './_components/InProgressCourseList';
-import GlobalApi from '@/app/_utils/GlobalApi';
+"use client";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
+import SideBanners from "../courses/_components/SideBanners";
+import WelcomeBannerDashboard from "./_components/WelcomeBannerDashboard";
+import InProgressCourseList from "./_components/InProgressCourseList";
+import GlobalApi from "@/app/_utils/GlobalApi";
 
 function Dashboard() {
-  const {user}=useUser();
-  const [userEnrolledCourses,setUserEnrolledCourse]=useState([]);
-  useEffect(()=>{
-    user&&getAllUserEnrolledCourses();
-  },[user])
+  const { user } = useUser();
+  const [userEnrolledCourses, setUserEnrolledCourse] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      getAllUserEnrolledCourses();
+    }
+  }, [user]);
 
   /**
    * Get All User Enrolled Course List
    */
-  const getAllUserEnrolledCourses=()=>{
-    GlobalApi.getUserAllEnrolledCourseList(user.primaryEmailAddress.emailAddress).then(resp=>{
+  const getAllUserEnrolledCourses = () => {
+    GlobalApi.getUserAllEnrolledCourseList(
+      user.primaryEmailAddress.emailAddress
+    ).then((resp) => {
       console.log(resp);
       setUserEnrolledCourse(resp.userEnrollCourses);
-    })
-  }
+    });
+  };
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-4 p-5 gap-5'>
-      {/* Left Container  */}
-      <div className='col-span-3'>
-          {/* Banner  */}
-            <WelcomeBannerDashboard user={user}/>
-          {/* In Progress Course List  */}
-            <InProgressCourseList userEnrolledCourses={userEnrolledCourses}/>
-        
-      </div>
-      {/* Right Container */}
-      <div className=' '>
-        <SideBanners/>
+    <div className="p-4 space-y-5">
+      {/* Banner */}
+      <WelcomeBannerDashboard user={user} />
+
+      {/* Grid for content and side banners */}
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+        {/* Left/Main content */}
+        <div className="lg:col-span-3 space-y-5">
+          <InProgressCourseList userEnrolledCourses={userEnrolledCourses} />
+        </div>
+
+        {/* Right/sidebar */}
+        <div>
+          <SideBanners />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
